fd1badd92175cde6b7f9546d5cdb37dd2a0c058e
Abhijeetdutt <abhijeetduttpandey@gmail.com>
2020-07-28 18:10:36 +0530

    Update remote_pattern_finder.py -- added debug parameter ( --debug=True )
    Update remote_pattern_finder.py -- added debug parameter ( --debug=True )

--- remote_pattern_finder.py
+++ remote_pattern_finder.py
@@ -9,6 +9,8 @@ class Solution :
                         print("Only works for Linux machines")
                 else :
                         command = 'ssh ' + login + ' " if [[ -n \$(ls ' + dictionary[login][1] + ' 2>/dev/null) ]]; then zgrep \\\"' + dictionary[login][0] + '\\\" ' + dictionary[login][1] + ' | sed \\\"s/^/\$(whoami)@\$(hostname) /g\\\"; fi"'
+                        if dictionary[login][2] :
+                                print(" [[ DEBUG ]] " + str(command))
                         result = subprocess.check_output(command,shell=True).strip()
                         if result : print(result)
 
@@ -33,19 +35,24 @@ if __name__ == '__main__' :
                         help='pattern : "2020-07-28.*Bad"')
         parser.add_argument('--file_path', metavar='file_path', required=True,
                         help='file_path : "2020-07-28.*Bad"')
+        parser.add_argument('--debug', metavar='debug',
+                        help='debug : "True"')
 
         args = parser.parse_args()
 
         envfile = str(args.env_file)
         pattern = str(args.pattern)
         filepath = str(args.file_path)
+        debug = False
+        if args.debug :
+                debug = str(args.debug)
 
         global dictionary
         dictionary={}
 
         with open(envfile,'r') as f :
                 for login in f :
-                        dictionary[login.strip()] = (pattern,filepath)
+                        dictionary[login.strip()] = (pattern,filepath,debug)
 
         solution = Solution()
 
