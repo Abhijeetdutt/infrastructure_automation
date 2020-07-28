df376a199597ffaf439916ae2e721506a64089fc
Abhijeetdutt <abhijeetduttpandey@gmail.com>
2020-07-28 14:19:32 +0530

    Create remote_pattern_finder.py
    /usr/bin/python2.7 pattern_finder.py \
    --env_file config/TBS.CM-PREPAID.LST \
    --file_path JEE/ABPProduct/config_files/ABP-FULL/ABPServer/uams.properties \
    --pattern "policy.account.lockout.time"

---
+++ remote_pattern_finder.py
@@ -0,0 +1,61 @@
+class Solution :
+
+        def init__(self) :
+                self.currPath  = os.getcwd()
+
+        def fetch_detail(self,login) :
+
+                if "linux" not in platform :
+                        print("Only works for Linux machines")
+                else :
+                        command = 'ssh ' + login + ' " if [[ -n \$(ls ' + dictionary[login][1] + ' 2>/dev/null) ]]; then zgrep \\\"' + dictionary[login][0] + '\\\" ' + dictionary[login][1] + ' | sed \\\"s/^/\$(whoami)@\$(hostname) /g\\\"; fi"'
+                        result = subprocess.check_output(command,shell=True).strip()
+                        if result : print(result)
+
+
+if __name__ == '__main__' :
+
+        import subprocess
+        import datetime
+        import time
+        import sys,os
+        from multiprocessing import Process, current_process, Lock, Manager
+        from multiprocessing.dummy import Pool as ThreadPool
+        import json
+        from sys import platform
+        import argparse
+        from datetime import datetime
+
+        parser = argparse.ArgumentParser(description='pass the parameters - pattern/file_path/environment-sheet')
+        parser.add_argument('--env_file', metavar='env_file', required=True,
+                        help='env_file : environment_file.txt')
+        parser.add_argument('--pattern', metavar='pattern', required=True,
+                        help='pattern : "2020-07-28.*Bad"')
+        parser.add_argument('--file_path', metavar='file_path', required=True,
+                        help='file_path : "2020-07-28.*Bad"')
+
+        args = parser.parse_args()
+
+        envfile = str(args.env_file)
+        pattern = str(args.pattern)
+        filepath = str(args.file_path)
+
+        global dictionary
+        dictionary={}
+
+        with open(envfile,'r') as f :
+                for login in f :
+                        dictionary[login.strip()] = (pattern,filepath)
+
+        solution = Solution()
+
+        print("Executing the program for : " + str(envfile) + "\n")
+        print("Start Time =", datetime.now().strftime("%H:%M:%S"))
+        print("\nData found : \n")
+        pool = ThreadPool(50)
+        results = pool.map(solution.fetch_detail, dictionary.keys())
+        pool.close()
+        pool.join()
+        print("\n")
+        print("End Time =", datetime.now().strftime("%H:%M:%S"))
+        print("\n")
